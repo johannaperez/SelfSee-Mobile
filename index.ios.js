0fee7d1a36aca84e6aeff34ec70a6e251a9a3f1e
johanna08 <johannaperez@JohannasMacBook.home>
2016-09-25 20:48:15 -0400

    all buttons made

--- index.ios.js
+++ index.ios.js
@@ -10,92 +10,15 @@ import {
   TouchableHighlight
 } from 'react-native';
 import Camera from 'react-native-camera';
-import {Takepic} from './ios/components/camera';
-const {
-  MKButton,
-  MKColor,
-} = MK;
-const Fab = MKButton.coloredFab()
-  .withBackgroundColor(MKColor.Yellow)
-  .build();
+import { Takepic } from './ios/components/camera';
+import { Emotions } from './ios/components/buttons'
 
 class SelfSee extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          How are you feeling today?
-        </Text>
-        <View>
-          <View style={styles.row}>
-          <Fab style={styles.Buttonstyle} onPress={() => {
-                console.warn('hi, raised button!');
-                }}>
-                <Image pointerEvents="none" source={require('./happy.png')} />
-          </Fab>
-          <Fab style={styles.Buttonstyle} onPress={() => {
-                console.warn('hi, raised button!');
-                }}>
-                <Image pointerEvents="none" source={require('./sad.png')} />
-          </Fab>
-          </View>
-          <View style={styles.row}>
-          <Fab style={styles.Buttonstyle} onPress={() => {
-                console.warn('hi, raised button!');
-                }}>
-                <Image pointerEvents="none" source={require('./surprise.png')} />
-          </Fab>
-          <Fab style={styles.Buttonstyle} onPress={() => {
-                console.warn('hi, raised button!');
-                }}>
-                <Image pointerEvents="none" source={require('./images/angry.png')} />
-          </Fab>
-          </View>
-          <View style={styles.row}>
-          <Fab style={styles.Buttonstyle} onPress={() => {
-                console.warn('hi, raised button!');
-                }}>
-                <Image pointerEvents="none" source={require('./disgusting.png')} />
-          </Fab>
-          <Fab style={styles.Buttonstyle} onPress={() => {
-                console.warn('hi, raised button!');
-                }}>
-                <Image pointerEvents="none" source={require('./images/fear.png')} />
-          </Fab>
-          </View>
-            <Text style={styles.Buttontext}>
-                SURPRISED
-            </Text>
-        </View>   
-      </View>
-    );
+      <Emotions />
+      )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  row: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center'
-  },
-  Buttonstyle:{
-     justifyContent: 'center',
-  },
-  Buttontext:{
-    textAlign: 'center',
-    color: '#000000',
-    fontSize: 10,
-  }
-});
 
 AppRegistry.registerComponent('SelfSee', () => SelfSee);
--- ios/components/buttons.js
+++ ios/components/buttons.js
@@ -1,46 +1,99 @@
-import React from 'react';
 const MK = require('react-native-material-kit');
-const appStyles = require('./styles');
-
+import React, { Component } from 'react';
 import {
+  AppRegistry,
+  Dimensions,
   StyleSheet,
   Text,
-  View,
-  ScrollView,
   Image,
+  View,
+  TouchableHighlight
 } from 'react-native';
+import { Takepic } from './ios/components/camera';
 
 const {
   MKButton,
   MKColor,
 } = MK;
-
-import { setTheme, MKColor, Button } from 'react-native-material-kit';
-
-// customize the material design theme
-setTheme({
-  primaryColor: MKColor.BlueGrey,
-  primaryColorRGB: MKColor.RGBIndigo,
-  accentColor: MKColor.Grey,
-});
-
-const styles = Object.assign({}, appStyles, StyleSheet.create({
-  buttonText: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  fab: {
-    width: 200,
-    height: 200,
-    borderRadius: 100,
-  },
-}));
-
-const PlainFab = MKButton.plainFab()
-  .withText('BUTTON')
-  .withOnPress(() => {
-    console.log("Hi, it's a colored button!");
-  })
-  .withStyle(styles.fab)
+const Fab = MKButton.coloredFab()
+  .withBackgroundColor(MKColor.Yellow)
   .build();
+
+export default class Emotions extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          How are you feeling today?
+        </Text>
+        <View>
+          <View style={styles.row}>
+          <Fab style={styles.Buttonstyle} onPress={() => {
+                console.warn('hi, raised button!');
+                }}>
+                <Image pointerEvents="none" source={require('./happy.png')} />
+          </Fab>
+          <Fab style={styles.Buttonstyle} onPress={() => {
+                console.warn('hi, raised button!');
+                }}>
+                <Image pointerEvents="none" source={require('./sad.png')} />
+          </Fab>
+          </View>
+          <View style={styles.row}>
+          <Fab style={styles.Buttonstyle} onPress={() => {
+                console.warn('hi, raised button!');
+                }}>
+                <Image pointerEvents="none" source={require('./surprise.png')} />
+          </Fab>
+          <Fab style={styles.Buttonstyle} onPress={() => {
+                console.warn('hi, raised button!');
+                }}>
+                <Image pointerEvents="none" source={require('./images/angry.png')} />
+          </Fab>
+          </View>
+          <View style={styles.row}>
+          <Fab style={styles.Buttonstyle} onPress={() => {
+                console.warn('hi, raised button!');
+                }}>
+                <Image pointerEvents="none" source={require('./disgusting.png')} />
+          </Fab>
+          <Fab style={styles.Buttonstyle} onPress={() => {
+                console.warn('hi, raised button!');
+                }}>
+                <Image pointerEvents="none" source={require('./images/fear.png')} />
+          </Fab>
+          </View>
+            <Text style={styles.Buttontext}>
+                SURPRISED
+            </Text>
+        </View>   
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  },
+  Buttonstyle:{
+     justifyContent: 'center',
+  },
+  Buttontext:{
+    textAlign: 'center',
+    color: '#000000',
+    fontSize: 10,
+  }
+});
